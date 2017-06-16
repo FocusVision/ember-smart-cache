694fddee52db7213f5039effc8503d0b2d8d48fa
Kyle Mellander <kmellander@focusvision.com>
2017-06-16 14:02:17 -0700

    Track number of background loading models

--- addon/services/smart-model-update.js
+++ addon/services/smart-model-update.js
@@ -8,9 +8,19 @@ const {
 export default Service.extend({
   store: service(),
 
+  loading: computed.gt(backgroundLoads, 0),
+  backgroundLoads: 0,
+
   init() {
     navigator.serviceWorker.addEventListener('message', event => {
-      this.store.push(event.data)
+      if (event.data.loading) {
+        return this.incrementProperty('backgroundLoads')
+      }
+
+      if (event.data.type && event.data.id) {
+        this.store.push(event.data)
+        this.decrementProperty('backgroundLoads')
+      }
     })
   }
 })
