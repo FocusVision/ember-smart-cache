12fcf588cdce83776398c95553c656e4b7a89ff4
Kyle Mellander <kmellander@focusvision.com>
2017-06-19 13:37:35 -0700

    [RE-4393] Add updater to send changes to cache

--- addon/initializers/setup-model.js
+++ addon/initializers/setup-model.js
@@ -1,16 +1,16 @@
 import DS from 'ember-data'
 
 export function initialize(application) {
-  application.inject('route:application', 'smartModelUpdate', 'service:smart-model-update')
+  application.inject('route:application', 'smartModel', 'service:smart-model')
 
   DS.Model.reopen({
     set(key, value) {
       const result = this._super(key, value)
       const id = this.get('id')
       if (navigator.serviceWorker && navigator.serviceWorker.controller && id) {
-        const data = this.serialize()
-        data.id = id
-        navigator.serviceWorker.controller.postMessage(data)
+        const payload = this.serialize().data
+        payload.id = id
+        navigator.serviceWorker.controller.postMessage(payload)
       }
 
       return result
